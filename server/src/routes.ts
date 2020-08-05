8d452ce185e89b6a5a159bae5290e6a8d0b71971
doctorget <samuelsoares177778@gmail.com>
2020-08-05 17:04:47 -0300

    finalizando api

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,16 @@
 import express, { request } from 'express';
 import ClassesController from './controllers/ClassesController';
+import ConnectionController from './controllers/ConnectionsController';
 
 
 const routes = express.Router();
 const classesControler = new ClassesController();
+const connectionsController = new ConnectionController()
 
 routes.post('/classes',classesControler.create);
+routes.get('/classes', classesControler.index);
+
+routes.post('/connections', connectionsController.create);
+routes.get('/connections', connectionsController.index);
 
 export default routes;
